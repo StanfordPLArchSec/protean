b53c4da828b18afb1222a56fa4804c40bde2a7d7
Nicholas Mosier <nmosier@stanford.edu>
2023-10-12 00:41:47 +0000

    update

--- tools/ShadowDeclassificationTable.h
+++ tools/ShadowDeclassificationTable.h
@@ -2,6 +2,8 @@
 #include <array>
 #include <memory>
 #include <vector>
+#include <optional>
+#include <list>
 
 template <typename T>
 constexpr T div_up(T numer, T denom) {
@@ -186,4 +188,131 @@ private:
 #if 0
   ShadowMemory<int, 12, 0> shadow_declassified_pages(shadow_declassified_pages.ValueSize);
 #endif
+};
+
+
+class FiniteLRUShadowDeclassificationTable {
+public:
+  using LRUList = std::list<ADDRINT>;
+  using LRUIt = LRUList::iterator;
+  
+  FiniteLRUShadowDeclassificationTable(unsigned max_size): maxSize(max_size), shadow(std::nullopt) {}
+
+  bool checkDeclassified(ADDRINT addr, unsigned size) {
+    std::optional<LRUIt> *ptr = &shadow[addr];
+    const bool declassified = std::all_of(ptr, ptr + size, [] (const auto& opt) -> bool {
+      return static_cast<bool>(opt);
+    });
+    if (declassified) {
+      // Mark all declassification bits as used.
+      for (unsigned off = 0; off < size; ++off) {
+	LRUIt& lru_it = *ptr[off];
+	lru.erase(lru_it);
+	lru.push_front(addr + off);
+	lru_it = lru.begin();
+      }
+    }
+    return declassified;
+  }
+
+  void setDeclassified(ADDRINT addr, unsigned size) {
+    std::optional<LRUIt> *ptr = &shadow[addr];
+    for (unsigned off = 0; off < size; ++off) {
+      if (!ptr[off]) {
+	// Evict if necessary
+	if (lru.size() >= maxSize) {
+	  const ADDRINT evict_addr = lru.back();
+	  lru.pop_back();
+	  shadow[evict_addr] = std::nullopt;
+	}
+	lru.push_front(addr + off);
+	ptr[off] = lru.begin();
+      }
+    }
+  }
+
+  void setClassified(ADDRINT addr, unsigned size, ADDRINT) {
+    std::optional<LRUIt> *ptr = &shadow[addr];
+    for (unsigned off = 0; off < size; ++off) {
+      if (std::optional<LRUIt>& lru_it = ptr[off]) {
+	lru.erase(*lru_it);
+	lru_it = std::nullopt;
+      }
+    }
+  }
+
+  void printDesc(std::ostream& os) {
+    os << "finite-sized shadow declassification table, max_size=" << maxSize << "\n";
+  }
+
+  void printStats(std::ostream& os) {}
+  
+private:
+  unsigned maxSize;
+  ShadowMemory<std::optional<LRUIt>, 0, 12> shadow;
+  LRUList lru;
+};
+
+
+class FiniteRandShadowDeclassificationTable {
+public:
+  using Addrs = std::vector<ADDRINT>;
+
+  FiniteRandShadowDeclassificationTable(unsigned max_size): maxSize(max_size), shadow(nullptr) {
+    addrs.resize(max_size);
+    for (unsigned i = 0; i < max_size; ++i)
+      free.push_back(&addrs[i]);
+  }
+
+  bool checkDeclassified(ADDRINT addr, unsigned size) {
+    ADDRINT **ptr = &shadow[addr];
+    return std::transform_reduce(ptr, ptr + size, true,
+				 std::logical_and<bool>(),
+				 [] (ADDRINT *x) -> bool { return x != nullptr; });
+  }
+
+  void setDeclassified(ADDRINT addr, unsigned size) {
+    ADDRINT **ptr = &shadow[addr];
+    for (unsigned off = 0; off < size; ++off) {
+      ADDRINT* &val = ptr[off];
+      if (val == nullptr) {
+	assert(addrs.size() <= maxSize);
+	if (free.empty()) {
+	  // If the free list is empty, then we evict an existing address and push it to the free list.
+	  const unsigned evicted_idx = std::rand() % addrs.size();
+	  ADDRINT *evicted_addr_ptr = &addrs[evicted_idx];
+	  free.push_back(evicted_addr_ptr);
+	  shadow[*evicted_addr_ptr] = nullptr;
+	}
+	assert(!free.empty());
+	val = free.back();
+	*val = addr + off;
+	free.pop_back();
+      }
+    }
+  }
+
+  void setClassified(ADDRINT addr, unsigned size, ADDRINT) {
+    ADDRINT **ptr = &shadow[addr];
+    for (unsigned off = 0; off < size; ++off) {
+      ADDRINT* &val = ptr[off];
+      if (val) {
+	// Add to free list and zero.
+	free.push_back(val);
+	val = nullptr;
+      }
+    }
+  }
+
+  void printStats(std::ostream& os) {}
+
+  void printDesc(std::ostream& os) {
+    os << "finite random-evicted shadow declassification table with " << maxSize << " entries\n";
+  }
+  
+private:
+  unsigned maxSize;
+  ShadowMemory<ADDRINT *, 0, 12> shadow;
+  std::vector<ADDRINT *> free;
+  std::vector<ADDRINT> addrs;
 };
