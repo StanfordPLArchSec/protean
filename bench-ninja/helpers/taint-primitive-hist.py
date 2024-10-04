06f1a845a38352bd2caf5b11a7f963b90369f922
Nicholas Mosier <nmosier@stanford.edu>
2024-10-04 14:39:51 -0700

    update

--- bench-ninja/helpers/taint-primitive-hist.py
+++ bench-ninja/helpers/taint-primitive-hist.py
@@ -6,46 +6,70 @@ import argparse
 import gzip
 import operator
 import collections
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dbgout', nargs = '*')
 parser.add_argument('-n', '--count', type = int)
-parser.add_argument('--reg', '-r', action='store_true')
-parser.add_argument('--mem', '-m', action='store_true')
-parser.add_argument('--xmit', '-x', action='store_true')
+parser.add_argument('--reg', '-r', default=True)
+parser.add_argument('--mem', '-m', default=True)
+parser.add_argument('--xmit', '-x', default=True)
+parser.add_argument('-w', '--weights', type=str)
 args = parser.parse_args()
+
+weight_dict = dict()
+with open(args.weights) as f:
+    for line in f:
+        tokens = line.split()
+        w = float(tokens[0])
+        i = int(tokens[1])
+        weight_dict[i] = w
 
 def open_dbgout(path: str):
     if path.endswith('.gz'):
-        return gzip.open(path, 'rt')
+        f = gzip.open(path, 'rt')
     else:
-        return open(path)
+        f = open(path)
+    if args.weights:
+        cpt_idx = int(path.split('/')[-3])
+        weight = weight_dict[cpt_idx]
+    else:
+        weight = 1
+    return f, weight
 
-files = [open_dbgout(path) for path in args.dbgout]
+weighted_files = [open_dbgout(path) for path in args.dbgout]
 
 # 70915340918500: system.switch_cpus: TPE m-taint 0x271bdf   MOV_R_M : ld   ecx, SS:[t0 + rsp + 0x4]
-histo = collections.defaultdict(int)
+histos = dict()
+r_histo = collections.defaultdict(int)
+m_histo = collections.defaultdict(int)
+x_histo = collections.defaultdict(int)
 asms = dict()
-for f in files:
+for f, w in weighted_files:
     for line in f:
         tokens = line.split()
         if len(tokens) < 5 or tokens[2] != 'TPE':
             continue
         tp = tokens[3]
-        if (args.reg and tp == 'r-taint') or \
-           (args.mem and tp == 'm-taint') or \
-           (args.xmit and tp == 'x-taint'):
-            inst_addr = tokens[4]
-            histo[inst_addr] += 1
-            asms[inst_addr] = tokens[6]
+        inst_addr = tokens[4]
+        asms[inst_addr] = tokens[6]
+        if args.reg and tp == 'r-taint':
+            r_histo[inst_addr] += w
+        if args.mem and tp == 'm-taint':
+            m_histo[inst_addr] += w
+        if args.xmit and tp == 'x-taint':
+            x_histo[inst_addr] += w
 
 # At the end, print the top n.
-sorted_histo = sorted(histo.items(), key = operator.itemgetter(1), reverse = True)
+def tag_histo(tag, histo):
+    return [(*item, tag) for item in histo.items()]
+tagged_histo = tag_histo('r', r_histo) + tag_histo('m', m_histo) + tag_histo('x', x_histo)
+sorted_histo = sorted(tagged_histo, key = operator.itemgetter(1), reverse = True)
 n = len(sorted_histo)
 if args.count:
     n = args.count
 for i in range(n):
-    inst_addr, count = sorted_histo[i]
+    inst_addr, count, tag = sorted_histo[i]
     asm = asms[inst_addr]
-    print(count, inst_addr, asm)
+    print(count, inst_addr, tag, asm)
     
