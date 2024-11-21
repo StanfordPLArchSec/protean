e0128306f5436787f31d488e52d7df05d33197a7
Nicholas Mosier <nmosier@stanford.edu>
2024-11-20 19:33:00 -0800

    commit before changing

--- cryptobench/shared.sh
+++ cryptobench/shared.sh
@@ -5,7 +5,7 @@ build() {
 }
 
 # paths
-gem5_exe=../bench-ninja/sim/base/build/X86/gem5.opt
+gem5_exe=../bench-ninja/sim/base/build/X86_MESI_Three_Level/gem5.fast
 gem5_config=../gem5/configs/deprecated/example/se.py
 
 pin_exe=../pin/pin
@@ -26,17 +26,21 @@ djbsort_args=()
 benchmarks=(bearssl ctaes djbsort)
 
 # config
-interval=10000000
-warmup=50000000
+interval=50000000
+warmup=10000000
 num_simpoints=10
 
 # unsafe_args=(--cpu-type=X86O3CPU --caches --l1d_size=32kB --l1d_assoc=8 --l1i_size=32kB --l1i_assoc=8 --l2_size=256kB --l2_assoc=16 --l3_size=2mB --l3_assoc=16)
-unsafe_exe=../bench-ninja/sim/base/build/X86/gem5.opt
-unsafe_config=../gem5-spec2017/configs/deprecated/example/se.py
+unsafe_exe=../bench-ninja/sim/base/build/X86_MESI_Three_Level/gem5.fast
+unsafe_config=../gem5-spec2017/configs/AlderLake/se.py
 unsafe_args=()
 
-tpe_exe=../bench-ninja/sim/llsct/build/X86/gem5.opt
-tpe_config=../gem5/configs/deprecated/example/se.py
-tpe_args=(--tpe-mem=1 --tpe-reg=1 --llsct-declassify-impl=cache --llsct-declassify-lines=1024)
+tpe_exe=../bench-ninja/sim/llsct/build/X86_MESI_Three_Level/gem5.fast
+tpe_config=../gem5/configs/AlderLake/se.py
+tpe_args=(--tpe-mem=1 --tpe-reg=1 --llsct-declassify-impl=shadowl1)
 
-experiments=(unsafe tpe)
+spt_exe=../bench-ninja/sim/spt/build/X86_MESI_Three_Level/gem5.fast
+spt_config=../spt/configs/AlderLake/se.py
+spt_args=(--enableSPT=1 --moreTransmitInsts=2)
+
+experiments=(unsafe tpe spt)
