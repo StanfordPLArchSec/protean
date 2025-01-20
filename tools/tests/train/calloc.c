5bfde25aa1075845329c860dee719b85c4ef9f94
Nicholas Mosier <nmosier@stanford.edu>
2025-01-20 12:06:55 -0800

    update

--- tools/tests/train/calloc.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main() {
-  void *p = calloc(100, 100);
-  printf("%p\n", p);
-  free(p);
-}
--- tools/tests/train/reallocarray.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-  void *p = calloc(100, 100);
-  p = reallocarray(p, 102, 102);
-  p = reallocarray(p, 1, 1);
-  free(p);
-}
