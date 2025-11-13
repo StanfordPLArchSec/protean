ba1f77fe0f8d7a4cc58cef415ea18599060af6a5
Nicholas Hunter Mosier <nmosier@sh02-01n58.int>
2025-11-13 13:50:47 -0800

    remove old files

--- profiling-and-generating-bbv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_path=$(realpath ${BASH_SOURCE[0]})
-root_dir=$(dirname ${script_path})
-
-gem5_bin=${root_dir}/gem5/build/X86/gem5.fast
-se_py=${root_dir}/gem5/configs/deprecated/exmaple/se.py
