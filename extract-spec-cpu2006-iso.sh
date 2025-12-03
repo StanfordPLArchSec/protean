2a1c825c982bd612fcf8ffda003e1edbb492d7e0
Nicholas Mosier <nmosier@stanford.edu>
2025-12-02 19:46:58 -0800

    add spec cpu2006/cpu2017 extraction routines

---
+++ extract-spec-cpu2006-iso.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+usage() {
+    cat <<EOF
+usage: $0 /path/to/cpu2017-1.1.0.iso
+EOF
+}
+
+if [[ $# -ne 1 ]]; then
+    usage >&2
+    exit 1
+fi
+
+root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+iso="$(realpath "$1")"
+
+cd "$root"
+rm -rf cpu2006 cpu2006-install
+mkdir cpu2006-install
+cd cpu2006-install
+bsdtar -xf "$iso"
+chmod -R u+w .
+./install.sh -f -d ../cpu2006
---
+++ extract-spec-cpu2017-iso.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+usage() {
+    cat <<EOF
+usage: $0 /path/to/cpu2017-1.1.0.iso
+EOF
+}
+
+if [[ $# -ne 1 ]]; then
+    usage >&2
+    exit 1
+fi
+
+root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+iso="$(realpath "$1")"
+
+cd "$root"
+rm -rf cpu2017 cpu2017-install
+mkdir cpu2017-install
+cd cpu2017-install
+bsdtar -xf "$iso"
+chmod -R u+w .
+./install.sh -f -d ../cpu2017
