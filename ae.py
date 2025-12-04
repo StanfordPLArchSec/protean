525c6ef08852ccec22589a4b38e0bdcc5ef80f5c
Nicholas Mosier <nmosier@stanford.edu>
2025-12-04 07:52:02 -0800

    update

--- ae.py
+++ ae.py
@@ -12,13 +12,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--type", "-t", required=True, choices=["perf", "sec"])
 parser.add_argument("--size", "-s", required=True, choices=["small", "medium", "full"])
 parser.add_argument("--dry-run", "-n", action="store_true")
-parser.add_argument("snakemake_command", nargs="*", default=["snakemake"],
+parser.add_argument("--expected", action="store_true")
+parser.add_argument("snakemake_command", nargs="*", default=["snakemake", "--cores=all"],
                     help="Override snakemake binary/args with this.")
 args = parser.parse_args()
 
 # Go to the root directory.
 root_dir = Path(__file__).resolve().parent
 os.chdir(root_dir)
+
+def ResultPath(path):
+    path = Path(path)
+    if args.expected:
+        assert path.parts[0] != "reference"
+        path = Path("reference") / path
+    return path
 
 def do_perf_eval_small(args):
     # Run snakemake.
@@ -33,7 +41,7 @@ def do_perf_eval_small(args):
             return f"{self.prefix}/{conf}.pcore/results.json"
         def stat(self, conf):
             path = self.target(conf)
-            j = json.loads(Path(path).read_text())
+            j = json.loads(ResultPath(path).read_text())
             return j["stats"]["cycles"]
         def confs(self):
             return [
@@ -52,7 +60,7 @@ def do_perf_eval_small(args):
             return f"{self.prefix}/{conf}/stamp.txt"
         def stat(self, conf):
             l = []
-            with Path(self.target(conf)).with_name("stats.txt").open() as f:
+            with ResultPath(self.target(conf)).with_name("stats.txt").open() as f:
                 for line in f:
                     if m := re.match(r"simSeconds\s+([0-9.]+)", line):
                         l.append(float(m.group(1)))
@@ -83,7 +91,7 @@ def do_perf_eval_small(args):
         targets.extend(map(bench.target, bench.confs()))
 
     # Invoke snakemake to run the experiments.
-    if not args.dry_run:
+    if not args.dry_run and not args.expected:
         subprocess.run(args.snakemake_command + targets, check=True, shell=True)
 
     # Grab the results and print a table, similar to Table IV in the paper.
@@ -98,7 +106,6 @@ def do_perf_eval_small(args):
         ])
 
     # Pretty-print the table.
-    print("Table IV")
     print(tabulate(table))
 
 def do_perf_eval(args):
